d87f3759b36587d881d6ae1b2948f6b7dcb9979a
Eduardo Fiorentin <eduardoviniciusfiorentin@gmail.com>
2024-09-13 21:59:53 -0300

    upt

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+let messages = [
+    "Você deixa meu dia mais leve só de estar por perto",
+    "Adoro o jeito que você faz tudo ficar mais bonito",
+    "Você tem o sorriso mais lindo que já vi",
+    "Seu jeito me encanta mais a cada dia",
+    "Não importa o quão nublados sejam os dias, sua luz interior sempre brilha mais forte!",
+    "Cada vez que você sorri, eu esqueço o que ia dizer... É meio difícil me concentrar perto de você",
+    "Eu estava tentando encontrar algo perfeito para dizer, mas aí percebi que só você já é perfeita",
+    "Se eu pudesse escolher qualquer lugar para estar agora, seria ao seu lado",
+    "Você tem um jeitinho que faz meu coração bater mais rápido toda vez que te vejo",
+    "Se o seu sorriso fosse uma música, eu colocaria no repeat para sempre"
+]
+const idx_msg = Math.floor(Math.random() * messages.length);
+
+let emogis = [
+    "&#128513;",
+    "&#128522;",
+    "&#128541;",
+    "&#128579;",
+    "&#129321;",
+    "&#129392;",
+    "&#128517;"
+]
+const idx_emg = Math.floor(Math.random() * emogis.length);
+
+let message = "Não há nada tão lindo quanto seus olhos"
+$(".message_text").html(`\"${messages[idx_msg]}\"`)
+$("#title").html(`Olááá${emogis[idx_emg]}`)
